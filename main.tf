bdf7c33184ae551f554e24bd389e8ef401407df9
robc-io <robc.io.opensource@gmail.com>
2019-09-09 23:52:08 -0700

    claening up

--- main.tf
+++ main.tf
@@ -8,13 +8,11 @@ terraform {
 locals {
   name = var.name
   common_tags = {
-    "Terraform"   = true
+    "Terraform" = true
     "Environment" = var.environment
   }
 
-  tags                   = merge(var.tags, local.common_tags)
-  terraform_state_bucket = "terraform-states-${data.aws_caller_identity.this.account_id}"
-  terraform_state_region = var.terraform_state_region
+  tags = merge(var.tags, local.common_tags)
 }
 
 // TODO: This needs to have some logic to import existing keys and export to anywhere. Right now we just do both and
@@ -28,7 +26,7 @@ resource "tls_private_key" "key" {
 resource "local_file" "key_priv" {
   //  count = "${var.local_public_key == "" ? 0 : 1}"
 
-  content  = tls_private_key.key.private_key_pem
+  content = tls_private_key.key.private_key_pem
   filename = "${path.module}/id_rsa"
 }
 
@@ -42,7 +40,8 @@ resource "null_resource" "key_chown" {
   triggers = {
     always_run = timestamp()
   }
-  depends_on = [local_file.key_priv]
+  depends_on = [
+    local_file.key_priv]
 }
 
 resource "null_resource" "key_gen" {
@@ -55,22 +54,24 @@ resource "null_resource" "key_gen" {
   triggers = {
     always_run = timestamp()
   }
-  depends_on = [local_file.key_priv]
+  depends_on = [
+    local_file.key_priv]
 }
 
 data "local_file" "key_gen" {
   //  count = "${var.local_public_key == "" ? 0 : 1}"
-  filename   = "${path.module}/id_rsa.pub"
-  depends_on = [null_resource.key_gen]
+  filename = "${path.module}/id_rsa.pub"
+  depends_on = [
+    null_resource.key_gen]
 }
 
 data "local_file" "key_local" {
-  count    = var.local_public_key == "" ? 0 : 1
+  count = var.local_public_key == "" ? 0 : 1
   filename = var.local_public_key
 }
 
 resource "aws_key_pair" "key" {
-  key_name   = local.name
+  key_name = local.name
   public_key = var.local_public_key == "" ? data.local_file.key_gen.content : data.local_file.key_local[0].content
 }
 

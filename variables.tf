e5854a7a63cabe4c5f9b8a67c4424bc6d47193b5
robc-io <robc.io.opensource@gmail.com>
2019-10-21 14:02:32 -0700

    cleaning up unused vars

--- variables.tf
+++ variables.tf
@@ -1,27 +1,20 @@
-variable "name" {}
-
-variable "region" {
-  description = "The region you are deploying into"
+variable "name" {
+  type = string
 }
 
 variable "environment" {
+  type = string
   description = "The environment that generally corresponds to the account you are deploying into."
 }
 
-//variable "resource_group" {
-//  description = "The grouping that is conventionally used to name resources"
-//}
-
 variable "tags" {
-  description = "Tags that are appended"
   type        = map(string)
-}
-
-variable "terraform_state_region" {
-  description = "AWS region used for Terraform states"
+  description = "Tags that are appended"
+  default = {}
 }
 
 variable "local_public_key" {
+  type = string
   description = "The file location of the key you wish to import.  If you want to generate a key then don't set."
 }
 
